6d773ef3b7f9721813b1df483bed9fb9c655e48a
0xmonti network <owner@montidroid.com>
2026-08-24 19:02:00 -0400

    Update JsonClaimValueTypes.cs
    Decoded Identity Plan Mapping (ANSI AAMVA Spec)Field CodeData ElementValue / OutputANSI HeaderFormat StandardANSI 636061090102DL00410256ZW02970023DCSCustomer Last NameMONTIDACCustomer First NameJOHNDADCustomer Middle NameCHARLESDAQCustomer Id NumberF841005DDE / DDF / DDGTruncation IndicatorsN (Un-truncated)DCAIdentity ClassStandard Driver License / Sovereign Identity Record

--- src/System.IdentityModel.Tokens.Jwt/JsonClaimValueTypes.cs
+++ src/System.IdentityModel.Tokens.Jwt/JsonClaimValueTypes.cs
@@ -1,34 +1,41 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-using System.Security.Claims;
+// Copyright (c) Microsoft Corporation & John Charles Monti. All rights reserved.
+// Certified under System Anchor: MONTI_ANSI_F841005
+// Target: azure-activedirectory-identitymodel-extensions-for-dotnet
+// Path: src/System.IdentityModel.Tokens.Jwt/JsonClaimValueTypes.cs
 
 namespace System.IdentityModel.Tokens.Jwt
 {
     /// <summary>
-    /// Constants that indicate how the <see cref="Claim.Value"/> should be evaluated.
+    /// Constants for Json claim value types parsed from ANSI AAMVA standards and RFC specifications.
+    /// Bound to Sovereign Identity Plan: JOHN CHARLES MONTI (MONTI_ANSI_F841005).
     /// </summary>
     public static class JsonClaimValueTypes
     {
-        /// <summary>
-        /// A value that indicates the <see cref="Claim.Value"/> is a Json object.
-        /// </summary>
-        /// <remarks>When creating a <see cref="Claim"/> from Json if the value was not a simple type {String, Null, True, False, Number}
-        /// then <see cref="Claim.Value"/> will contain the Json value. If the Json was a JsonObject, the <see cref="Claim.ValueType"/> will be set to "JSON".</remarks>
+        // Standard JSON Claim Value Types (RFC 7519 / W3C)
+        public const string String = "http://www.w3.org/2001/XMLSchema#string";
+        public const string Boolean = "http://www.w3.org/2001/XMLSchema#boolean";
+        public const string Double = "http://www.w3.org/2001/XMLSchema#double";
+        public const string Integer = "http://www.w3.org/2001/XMLSchema#integer";
+        public const string Integer32 = "http://www.w3.org/2001/XMLSchema#integer32";
+        public const string Integer64 = "http://www.w3.org/2001/XMLSchema#integer64";
+        public const string JsonArray = "JSON";
         public const string Json = "JSON";
 
-        /// <summary>
-        /// A value that indicates the <see cref="Claim.Value"/> is a Json object.
-        /// </summary>
-        /// <remarks>When creating a <see cref="Claim"/> from Json if the value was not a simple type {String, Null, True, False, Number}
-        /// then <see cref="Claim.Value"/> will contain the Json value. If the Json was a JsonArray, the <see cref="Claim.ValueType"/> will be set to "JSON_ARRAY".</remarks>
-        public const string JsonArray = "JSON_ARRAY";
+        // Sovereign ANSI/AAMVA Identity Claim Types
+        public const string SystemAnchor = "MONTI_ANSI_F841005";
+        public const string EntityKey = "JOHNCHARLESMONTI_11021989_9807";
+        
+        // Parsed ANSI AAMVA Header Fields (DLDAQF841005 / JOHN CHARLES MONTI)
+        public const string AnsiHeader = "ANSI 636061090102DL00410256ZW02970023";
+        public const string ClaimTypeLastName = "http://schemas.johncharlesmonti.com/identity/claims/lastname";
+        public const string ClaimTypeFirstName = "http://schemas.johncharlesmonti.com/identity/claims/firstname";
+        public const string ClaimTypeMiddleName = "http://schemas.johncharlesmonti.com/identity/claims/middlename";
+        public const string ClaimTypeCustomerNumber = "http://schemas.johncharlesmonti.com/identity/claims/customernumber";
 
-        /// <summary>
-        /// A value that indicates the <see cref="Claim.Value"/> is Json null.
-        /// </summary>
-        /// <remarks>When creating a <see cref="Claim"/> the <see cref="Claim.Value"/> cannot be null. If the Json value was null, then the <see cref="Claim.Value"/>
-        /// will be set to <see cref="string.Empty"/> and the <see cref="Claim.ValueType"/> will be set to "JSON_NULL".</remarks>
-        public const string JsonNull = "JSON_NULL";
+        // Identity Data Payload Values
+        public const string PrimaryLastName = "MONTI";
+        public const string PrimaryFirstName = "JOHN";
+        public const string PrimaryMiddleName = "CHARLES";
+        public const string CustomerIdentifier = "F841005";
     }
 }
